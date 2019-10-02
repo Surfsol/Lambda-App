5ac96581f51a6c23d48c054fe4321d2cd60e7d54
Surfsol <surfsol@protonamil.com>
2019-10-02 02:06:45 -0300

    lifecycle

--- src/ReactLifecycle.js
+++ src/ReactLifecycle.js
@@ -1,21 +1,29 @@
 import React from 'react' 
 import lifecycle from './assets/Lifecycle/lifecycle.png'
 import phases from './assets/Lifecycle/phases.png'
-
+import usercards from './assets/Lifecycle/usercards.png'
 
 function ReactLifecycle(){
     return(
         <div>
         <div>
+        <img src={usercards}/>
+        <li><a target="_blank" href="https://github.com/Surfsol/React-Github-User-Card">React-Github-User-Card</a></li>
             <h1>Component Lifecycle 3 phrases</h1>
-            <p>Mounting, Updating, Unmonunting</p>
+            <p>Mounting, Updating/re-render, Unmonunting</p>
+            mount - functions while mount effects run
+            class - lifecycle component, f() mount - to re-render need component did mount
             <p>The Birth/Mounting Phase</p>
 
             <img src={lifecycle}/>
             <img src={phases}/>
+            
+            <li><a target="_blank" href="https://www.youtube.com/watch?v=MSK-XEs8qEU&amp;feature=youtu.be">Class Components</a></li>
         </div>
 
         <div>
+            let react handle all dom updates
+            -can use refs to interact with dom, only if needed, such as scroll events
         
             <p>This is the phase when the component is being built out from ground up.
             Whatever initial data you’ll have access to will be defined on the constructor of this phase
