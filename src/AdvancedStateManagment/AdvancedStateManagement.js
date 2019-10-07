2fa1de7917f37819896417f1dc19c68f13e931c2
Surfsol <surfsol@protonamil.com>
2019-10-07 01:42:58 -0300

    Update ContextAPI.js

--- src/AdvancedStateManagment/AdvancedStateManagement.js
+++ src/AdvancedStateManagment/AdvancedStateManagement.js
@@ -5,24 +5,24 @@ import Redux from './Redux'
 import AsyncRedux from './AsyncRedux'
 import ReducerPattern from './ReducerPattern'
 
-function AdvancedStateManagement(){
+function AdvancedState(){
     return(
         <div>
             <div>
-            <NavLink to='/webapp2/AdvancedStateManagement/ContextAPI'>Context API</NavLink>
-            <Route path='/webapp2/AdvancedStateManagement/ContextAPI' component={ContextAPI}/>
+            <NavLink to='/webapp2/AdvancedState/ContextAPI'>Context API</NavLink>
+            <Route path='/webapp2/AdvancedState/ContextAPI' component={ContextAPI}/>
             </div>
             <div>
-            <NavLink to='/webapp2/AdvancedStateManagement/ReducerPattern'>Reducer Pattern</NavLink>
-            <Route path='/webapp2/AdvancedStateManagement/ReducerPattern' component={ReducerPattern}/>
+            <NavLink to='/webapp2/AdvancedState/ReducerPattern'>Reducer Pattern</NavLink>
+            <Route path='/webapp2/AdvancedState/ReducerPattern' component={ReducerPattern}/>
             </div>
             <div>
-            <NavLink to='/webapp2/AdvancedStateManagement/Redux'>Redux</NavLink>
-            <Route path='/webapp2/AdvancedStateManagement/Redux' component={Redux}/>
+            <NavLink to='/webapp2/AdvancedState/Redux'>Redux</NavLink>
+            <Route path='/webapp2/AdvancedState/Redux' component={Redux}/>
             </div>
             <div>
-            <NavLink to='/webapp2/AdvancedStateManagement/AsyncRedux'>Async Redux</NavLink>
-            <Route path='/webapp2/AdvancedStateManagement/AsyncRedux' component={AsyncRedux}/>
+            <NavLink to='/webapp2/AdvancedState/AsyncRedux'>Async Redux</NavLink>
+            <Route path='/webapp2/AdvancedState/AsyncRedux' component={AsyncRedux}/>
             </div>
         </div>
        
@@ -30,4 +30,4 @@ function AdvancedStateManagement(){
 
 }
 
-export default AdvancedStateManagement;
+export default AdvancedState;
--- src/AdvancedStateManagment/ContextAPI.js
+++ src/AdvancedStateManagment/ContextAPI.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import shopping from '../assets/AdvanceStateManagement/shopping.png'
+import step1 from '../assets/AdvanceStateManagement/step1.png'
 
 function ContextAPI(){
 
     return(
         <div>
+        <div><img src={shopping}/></div>
         <p>Eaiser to use than props, but not as scalable</p>
         <p>context</p>
+        <div><img src={step1}/>
+        <h4>Step 1, create context folder, add files, ProductContext.js</h4>
+        <h4>in App.js wrap return in ProductContext.Provider value=..</h4>
+        </div>
         </div>
     )
 }
