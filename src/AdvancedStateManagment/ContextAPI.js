e2a4592e42a037b2251bedbc410866e7215d33f0
Surfsol <surfsol@protonamil.com>
2019-10-07 15:13:17 -0300

    Update contextapi

--- src/AdvancedStateManagment/ContextAPI.js
+++ src/AdvancedStateManagment/ContextAPI.js
@@ -12,6 +12,15 @@ function ContextAPI(){
         <div><img src={step1}/>
         <h4>Step 1, create context folder, add files, ProductContext.js</h4>
         <h4>in App.js wrap return in ProductContext.Provider value=..</h4>
+        <h4>pass data we want to share, value={{}} </h4>
+        <h4>consume data in child component, ex. const product = useContext(ProductContext)</h4>
+        <div>
+            <li><a target="_blank" href="https://codesandbox.io/s/bold-montalcini-4t9um">Code Sandbox 2</a></li>
+        </div>
+        <div>
+            <li><a target="_blank" href="https://codesandbox.io/s/trinkets-httpajax-ii-solution-3tcoo">Code Sandbox 2</a></li>
+        </div>
+        
         </div>
         </div>
     )
